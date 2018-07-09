4c8e0127b2cc74ebde02c870dac93a5312798efc
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-07-09 11:35:08 -0400

    error, may be related to first item.

---
+++ Calc_two.rb
@@ -0,0 +1,4 @@
+def add(answer)
+  answer = first + second
+  answer
+end
--- test_calc.rb
+++ test_calc.rb
@@ -4,9 +4,16 @@ require_relative "Calc_two.rb"
 class Calculator < Minitest::Test
 
   def test_assert_values_turned_into_integer
-    assert_equal(1, add().class)
+    assert_equal(Integer, add().class)
   end
 
+  def test_assert_arguments
+    assert_equal(Integer, add().class)
+  end
+
+  def test_addition_answer
+    assert_equal(14, add(8, 6))
+  end
 
 
 
